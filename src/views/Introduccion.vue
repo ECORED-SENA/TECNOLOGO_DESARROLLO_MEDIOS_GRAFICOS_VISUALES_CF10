44f5157b523fb023e17734259d71b0ba8f2ed64f
elrilacho <80546125+elrilacho@users.noreply.github.com>
2021-12-20 04:56:03 -0500

    Ajustes video y material.zip

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
     p.mb-5 Apreciado aprendiz, le damos la bienvenida a este momento de aprendizaje orientado a conocer sobre las imágenes digitalizadas, su composición, manipulación y las herramientas que permiten sus retoques. Antes de empezar, le invitamos a revisar el siguiente video para identificar el contexto de aprendizaje.
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/ymSqw_W2vzs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/56_dSe6wVU8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
 
 
